833c68c3c7e389e7f292a03c23673d5c237ffadc
littlewizard <tanm0y@outlook.com>
2016-04-01 02:59:42 +0600

    first commit

---
+++ src/components/homePage.js
@@ -0,0 +1,14 @@
+"use strict"
+
+var React = require('react');
+var Home = React.createClass({
+	render: function() {
+		return (
+			<div className = "jumbotron">
+			  <h1>Exploring React</h1>
+			</div>
+			);
+	}
+});
+
+module.exports = Home;
